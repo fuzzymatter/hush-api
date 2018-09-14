f0b47610938d8598c6e099087ee1f4f601f8a6a9
Jake Klassen <jklassendev@gmail.com>
2018-09-13 23:07:21 -0400

    Fixed e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,10 +1,12 @@
-import request from 'supertest';
+import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
 import { AppModule } from './../src/app.module';
 import { INestApplication } from '@nestjs/common';
+import { MailerService } from '../src/mailer/mailer.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let mailerService: MailerService;
 
   beforeAll(async () => {
     const moduleFixture = await Test.createTestingModule({
@@ -12,16 +14,29 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    mailerService = moduleFixture.get<MailerService>(MailerService);
+
     await app.init();
   });
 
+  afterAll(async () => {
+    await app.close();
+  });
+
+  afterEach(() => jest.resetAllMocks());
+
   it('/signups (POST)', () => {
+    jest.spyOn(mailerService, 'send').mockImplementation(async () => {});
+
     return request(app.getHttpServer())
       .post('/signups')
       .send({
-        email: 'user@example.org',
+        email: 'jklassendev@gmail.com',
+        name: 'Jake Klassen',
       })
-      .expect(202)
-      .expect('user@example.org');
+      .expect(201)
+      .then(({ body }) => {
+        expect(body.id).toMatch(/.{20,}/);
+      });
   });
 });
