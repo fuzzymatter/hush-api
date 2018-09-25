87d8f4a1e69887dade33a8a5f38e2a2ebde206b5
Jake Klassen <jklassendev@gmail.com>
2018-09-25 07:48:42 -0400

    Added missing env variables to build-env script

--- scripts/build-env.ts
+++ scripts/build-env.ts
@@ -12,6 +12,7 @@ if (isCi === false) {
 }
 
 const content = `
+SENDGRID_API_KEY=${process.env.SENDGRID_API_KEY}
 TYPEORM_CONNECTION=${process.env.TYPEORM_CONNECTION}
 TYPEORM_HOST=${process.env.TYPEORM_HOST}
 TYPEORM_PORT=${process.env.TYPEORM_PORT}
@@ -19,6 +20,10 @@ TYPEORM_USERNAME=${process.env.TYPEORM_USERNAME}
 TYPEORM_PASSWORD=${process.env.TYPEORM_PASSWORD}
 TYPEORM_DATABASE=${process.env.TYPEORM_DATABASE}
 TYPEORM_SYNCHRONIZE=${process.env.TYPEORM_SYNCHRONIZE}
+TYPEORM_LOGGING=${process.env.TYPEORM_LOGGING}
+TYPEORM_ENTITIES=${process.env.TYPEORM_ENTITIES}
+NODE_ENV=${process.env.NODE_ENV}
+PORT=${process.env.PORT}
 `;
 
 fs.writeFileSync(path.resolve(__dirname, '../', '.env'), content);
